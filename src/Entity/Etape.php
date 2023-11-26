29297e2c5509cd891ab9a1491613c1ec7f27ed83
Yasmina Bouhriz El Bouhlali <yasmina.bouhriz-el-bouhlali@etudiant.univ-reims.fr>
2023-11-26 01:44:10 +0100

    Création de l'entité Etape et de EtapeRepository avec l'utilisation de MarkerBundle

---
+++ src/Entity/Etape.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EtapeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EtapeRepository::class)]
+class Etape
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $numeroEtape = null;
+
+    #[ORM\Column(length: 500, nullable: true)]
+    private ?string $descriptionEtape = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumeroEtape(): ?int
+    {
+        return $this->numeroEtape;
+    }
+
+    public function setNumeroEtape(?int $numeroEtape): static
+    {
+        $this->numeroEtape = $numeroEtape;
+
+        return $this;
+    }
+
+    public function getDescriptionEtape(): ?string
+    {
+        return $this->descriptionEtape;
+    }
+
+    public function setDescriptionEtape(?string $descriptionEtape): static
+    {
+        $this->descriptionEtape = $descriptionEtape;
+
+        return $this;
+    }
+}
