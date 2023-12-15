21b5ab51fdd5b8c735b595ff0eac509695288097
line martinet <line.martinet@etudiant.univ-reims.fr>
2023-12-15 19:27:33 +0100

    mises en place de fixtures pour l'entité ingredient

--- src/DataFixtures/IngredientFixtures.php
+++ src/DataFixtures/IngredientFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\IngredientFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 ;
@@ -10,9 +11,6 @@ class IngredientFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        IngredientFactory::createMany(150);
     }
 }
