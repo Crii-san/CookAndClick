1fedb2ebd718bfbf7d8c04ba2e962bd7b8007966
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2023-12-24 12:37:30 +0100

    Modification de la génération de users aléatoires : ajout d'une probabilité d'allergène aléatoire

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,21 +2,39 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\AllergeneFactory;
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
 
 class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $faker = Faker\Factory::create();
+        $proba = $faker->boolean(90);
+        if ($proba) {
+            $allergene = AllergeneFactory::random();
+        } else {
+            $allergene = null;
+        }
+
         UserFactory::createOne(['nom' => 'Souka', 'prenom' => 'Christelle', 'email' => 'christelle@example.com', 'roles' => ['ROLE_ADMIN']]);
         UserFactory::createOne(['nom' => 'Martinet', 'prenom' => 'Line', 'email' => 'line@example.com', 'roles' => ['ROLE_ADMIN']]);
         UserFactory::createOne(['nom' => 'Bouhriz El Bouhlali', 'prenom' => 'Yasmina', 'email' => 'yasmina@example.com', 'roles' => ['ROLE_ADMIN']]);
         UserFactory::createOne(['nom' => 'Fontao', 'prenom' => 'Thomas', 'email' => 'thomas@example.com', 'roles' => ['ROLE_ADMIN']]);
-        for ($i = 0; $i < 10; ++$i) {
-            UserFactory::createOne();
-        }
+
+
+        UserFactory::createMany(10, function () {
+            $faker = Faker\Factory::create();
+            $proba = $faker->boolean(20);
+            if ($proba) {
+                return ['allergene' => AllergeneFactory::random()];
+            } else {
+                return ['allergene' => null];
+            }
+        });
 
         $manager->flush();
     }
