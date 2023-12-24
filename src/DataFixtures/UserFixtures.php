1ce7228d11676c1b1b33befce77ed45d80975d26
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2023-12-24 12:10:57 +0100

    Création de 4 users avec le rôle administrateur

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,13 +5,16 @@ namespace App\DataFixtures;
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-;
 
 class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 30; ++$i) {
+        UserFactory::createOne(['nom' => 'Souka', 'prenom' => 'Christelle', 'email' => 'christelle@example.com', 'roles' => ['ROLE_ADMIN']]);
+        UserFactory::createOne(['nom' => 'Martinet', 'prenom' => 'Line', 'email' => 'line@example.com', 'roles' => ['ROLE_ADMIN']]);
+        UserFactory::createOne(['nom' => 'Bouhriz El Bouhlali', 'prenom' => 'Yasmina', 'email' => 'yasmina@example.com', 'roles' => ['ROLE_ADMIN']]);
+        UserFactory::createOne(['nom' => 'Fontao', 'prenom' => 'Thomas', 'email' => 'thomas@example.com', 'roles' => ['ROLE_ADMIN']]);
+        for ($i = 0; $i < 10; ++$i) {
             UserFactory::createOne();
         }
 
