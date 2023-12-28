767d247db9687576fe2a85872bc6145f40039c83
Yasmina Bouhriz El Bouhlali <yasmina.bouhriz-el-bouhlali@etudiant.univ-reims.fr>
2023-12-28 01:13:37 +0100

    Modification d gégérateur de recette pour qu'il affecte une catégorie aléatoire à une recette générée

--- src/DataFixtures/RecetteFixtures.php
+++ src/DataFixtures/RecetteFixtures.php
@@ -2,24 +2,19 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\CategorieFactory;
 use App\Factory\RecetteFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-;
 
 class RecetteFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         RecetteFactory::createMany(100, function () {
-            $hasCategory = CategorieFactory::faker()->boolean(90);
-            if ($hasCategory) {
-                return [
-                    'categorie' => CategorieFactory::random(),
-                ];
-            } else {
-                return [];
-            }
+            return [
+                'categorie' => CategorieFactory::random(),
+            ];
         });
     }
 
