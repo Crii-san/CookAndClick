82d9760597dabc5dbfde68ab5d7ad26add8f048d
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2023-12-28 12:14:42 +0100

    Création de 50 ingrédients avec ou sans allergène

--- src/DataFixtures/IngredientFixtures.php
+++ src/DataFixtures/IngredientFixtures.php
@@ -2,15 +2,21 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\AllergeneFactory;
 use App\Factory\IngredientFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-;
 
 class IngredientFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        IngredientFactory::createMany(150);
+        for ($i = 1; $i <= 15; ++$i) {
+            $var = AllergeneFactory::random();
+            IngredientFactory::createOne(['allergenes' => [$var]]);
+        }
+        for ($i = 1; $i <= 35; ++$i) {
+            IngredientFactory::createOne();
+        }
     }
 }
