fc99ac5728c702e2829bb4f6f6bb0d762635263d
Yasmina Bouhriz El Bouhlali <yasmina.bouhriz-el-bouhlali@etudiant.univ-reims.fr>
2024-01-05 00:08:42 +0100

    (Tentative - ne fonctionne pas) Modification de la méthode load() et ajout de la méthode getDependencies() pour les dépendance entre Etape et Recette dans EtapeFixtures.php

--- src/DataFixtures/EtapeFixtures.php
+++ src/DataFixtures/EtapeFixtures.php
@@ -2,14 +2,26 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\EtapeFactory;
+use App\Factory\RecetteFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-;
 
 class EtapeFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        EtapeFactory::createMany(50, function () {
+            return [
+                'recette' => RecetteFactory::random(),
+            ];
+        });
+    }
 
+    public function getDependencies(): array
+    {
+        return [
+            RecetteFactory::class,
+        ];
     }
 }
