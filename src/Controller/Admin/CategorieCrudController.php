3edf211c2322a28dcd24bb7ed6dc0d3656612809
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-10 18:07:45 +0100

    Configuration de l'affichage des champs de l'entité Categorie

--- src/Controller/Admin/CategorieCrudController.php
+++ src/Controller/Admin/CategorieCrudController.php
@@ -3,9 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Categorie;
+use Doctrine\ORM\EntityRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CategorieCrudController extends AbstractCrudController
@@ -15,14 +16,19 @@ class CategorieCrudController extends AbstractCrudController
         return Categorie::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('nom'),
+            AssociationField::new('recettes')
+                ->setFormTypeOptions([
+                    'choice_label' => 'nom',
+                    'query_builder' => function (EntityRepository $entityRepository) {
+                        return $entityRepository->createQueryBuilder('r')
+                            ->orderBy('r.nom', 'ASC');
+                    },
+                ]),
         ];
     }
-    */
 }
