72dc99ab17cb83f96ee0ca154562061d2441e070
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-10 18:01:22 +0100

    Configuration de l'affichage des champs de l'entité Recette

--- src/Controller/Admin/RecetteCrudController.php
+++ src/Controller/Admin/RecetteCrudController.php
@@ -3,9 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Recette;
+use Doctrine\ORM\EntityRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class RecetteCrudController extends AbstractCrudController
@@ -15,14 +17,35 @@ class RecetteCrudController extends AbstractCrudController
         return Recette::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('nom'),
+            IntegerField::new('nivDifficulte'),
+            TextField::new('description'),
+            IntegerField::new('nbPersonne'),
+            IntegerField::new('minutes'),
+            IntegerField::new('heures'),
+            AssociationField::new('categorie')
+                ->setFormTypeOptions([
+                    'choice_label' => 'nom',
+                    'query_builder' => function (EntityRepository $entityRepository) {
+                        return $entityRepository->createQueryBuilder('c')
+                            ->orderBy('c.nom', 'ASC');
+                    },
+                ])
+                ->formatValue(function ($value) {
+                    return $value->getNom();
+                }),
+            AssociationField::new('etapes')
+                ->setFormTypeOptions([
+                    'choice_label' => 'numero',
+                    'query_builder' => function (EntityRepository $entityRepository) {
+                        return $entityRepository->createQueryBuilder('e')
+                            ->orderBy('e.numero', 'ASC');
+                    },
+                ]),
         ];
     }
-    */
 }
