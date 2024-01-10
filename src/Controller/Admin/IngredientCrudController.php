81315371de8c39b856a4158178cfc71ce1e843d0
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-10 18:17:23 +0100

    Configuration de l'affichage des champs de l'entité Ingredient

--- src/Controller/Admin/IngredientCrudController.php
+++ src/Controller/Admin/IngredientCrudController.php
@@ -3,8 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Ingredient;
+use Doctrine\ORM\EntityRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +18,24 @@ class IngredientCrudController extends AbstractCrudController
         return Ingredient::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('nom'),
+            IntegerField::new('calories'),
+            TextField::new('description'),
+            TextField::new('uniteMesure'),
+            AssociationField::new('allergenes')
+                ->setFormTypeOptions([
+                    'choice_label' => 'nom',
+                    'query_builder' => function (EntityRepository $entityRepository) {
+                        return $entityRepository->createQueryBuilder('a')
+                            ->orderBy('a.nom', 'ASC');
+                    },
+                ]),
         ];
     }
-    */
+
 }
