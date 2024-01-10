7f9ab3b478410d2a8d57c2f1ef29ca96edde9c1b
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-10 18:31:15 +0100

    Configuration de l'affichage des champs de l'entité Etape

--- src/Controller/Admin/EtapeCrudController.php
+++ src/Controller/Admin/EtapeCrudController.php
@@ -3,8 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Etape;
+use Doctrine\ORM\EntityRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +18,33 @@ class EtapeCrudController extends AbstractCrudController
         return Etape::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            IntegerField::new('numero'),
+            TextField::new('description'),
+            AssociationField::new('ingredients')
+                ->setFormTypeOptions([
+                    'choice_label' => 'nom',
+                    'query_builder' => function (EntityRepository $entityRepository) {
+                        return $entityRepository->createQueryBuilder('r')
+                            ->orderBy('r.nom', 'ASC');
+                    },
+                ]),
+            AssociationField::new('recette')
+                ->setFormTypeOptions([
+                    'choice_label' => 'nom',
+                    'query_builder' => function (EntityRepository $entityRepository) {
+                        return $entityRepository->createQueryBuilder('r')
+                            ->orderBy('r.nom', 'ASC');
+                    },
+                ])
+                ->formatValue(function ($value) {
+                    return $value?->getNom();
+                }),
         ];
     }
-    */
+
 }
