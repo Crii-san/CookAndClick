242b32057c9d0fc243d9298a639467428773107d
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-10 17:19:57 +0100

    Ajout des liens vers les « CRUD » des différentes entités

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Allergene;
+use App\Entity\Categorie;
+use App\Entity\Etape;
+use App\Entity\Ingredient;
+use App\Entity\Recette;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -19,12 +25,19 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Sae3 01');
+            ->setTitle('Cook & Click Admin');
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        return [
+            MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+            MenuItem::linkToCrud('Allergènes', 'fas fa-list', Allergene::class),
+            MenuItem::linkToCrud('Ingredients', 'fas fa-list', Ingredient::class),
+            MenuItem::linkToCrud('Catégories', 'fas fa-list', Categorie::class),
+            MenuItem::linkToCrud('Recette', 'fas fa-list', Recette::class),
+            MenuItem::linkToCrud('User', 'fas fa-list', User::class),
+            MenuItem::linkToCrud('Etape', 'fas fa-list', Etape::class),
+        ];
     }
 }
