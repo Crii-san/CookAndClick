bb2c718edcde6c6c00208b6e315040f8966a4cfa
Yasmina Bouhriz El Bouhlali <yasmina.bouhriz-el-bouhlali@etudiant.univ-reims.fr>
2024-01-11 16:16:43 +0100

    Changement de type des champs email et tel, et ajout de l'option empty_data

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,7 +7,9 @@ use App\Entity\User;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,18 +19,27 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+            'empty_data' => '',
+                ])
             ->add('password', PasswordType::class, [
                 'mapped' => false,
                 'required' => false,
                 ])
-            ->add('nom')
-            ->add('prenom')
+            ->add('nom', TextType::class, [
+                'empty_data' => '',
+            ])
+            ->add('prenom', TextType::class, [
+                'empty_data' => '',
+            ])
             ->add('dateNais')
             ->add('pseudo', TextType::class, [
                 'required' => false,
+                'empty_data' => '',
                     ])
-            ->add('tel')
+            ->add('tel', TelType::class, [
+                'empty_data' => '',
+            ])
             ->add('allergene', EntityType::class, [
                 'class' => Allergene::class,
                 'choice_label' => 'nom',
