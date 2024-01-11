165784f5038f526e9bfdf8724efa8265228ec182
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-11 16:53:59 +0100

    Correction fixture

--- src/DataFixtures/EtapeFixtures.php
+++ src/DataFixtures/EtapeFixtures.php
@@ -72,7 +72,7 @@ class EtapeFixtures extends Fixture implements DependentFixtureInterface
         // Etapes créées aléatoirement
         EtapeFactory::createMany(90, function () {
             $nbIngredients = rand(0, 5);
-            $nb = rand(4, 13);
+            $nb = rand(4, 12);
 
             $ingredients = [];
             for ($i = 0; $i < $nbIngredients; ++$i) {
