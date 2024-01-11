2389e5713ed488298935a41080cf47748ad94ce9
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-11 15:10:13 +0100

    Utilisation du fichier Json pour créer les étape de la recette Pizza

--- src/DataFixtures/EtapeFixtures.php
+++ src/DataFixtures/EtapeFixtures.php
@@ -4,17 +4,44 @@ namespace App\DataFixtures;
 
 use App\Factory\EtapeFactory;
 use App\Factory\IngredientFactory;
-use App\Factory\RecetteFactory;
+use App\Repository\RecetteRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
 class EtapeFixtures extends Fixture implements DependentFixtureInterface
 {
+    public function __construct(RecetteRepository $recetteRepository)
+    {
+        $this->recetteRepository = $recetteRepository;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        EtapeFactory::createMany(200, function () {
+        // Etapes de la recette Pizza
+        $repertoire = __DIR__;
+        $contenuFichier = file_get_contents("{$repertoire}\data\Etape_pizza.json");
+        $contenuFichierDecode = json_decode($contenuFichier);
+
+        $numero = 1;
+
+        foreach ($contenuFichierDecode as $element) {
+            $description = $element->description;
+
+            EtapeFactory::createOne(
+                ['description' => $description,
+                 'numero' => $numero,
+                    'recette' => $this->recetteRepository->find(3)]);
+            ++$numero;
+        }
+
+        // Etapes de la recette Salade
+        // Etapes de la recette Gateau au chocolat
+
+        // Etapes créées aléatoirement
+        EtapeFactory::createMany(90, function () {
             $nbIngredients = rand(0, 5);
+            $nb = rand(4, 13);
 
             $ingredients = [];
             for ($i = 0; $i < $nbIngredients; ++$i) {
@@ -22,7 +49,7 @@ class EtapeFixtures extends Fixture implements DependentFixtureInterface
             }
 
             return [
-                'recette' => RecetteFactory::random(),
+                'recette' => $this->recetteRepository->find($nb),
                 'ingredients' => $ingredients,
             ];
         });
