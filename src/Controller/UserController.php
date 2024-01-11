2d323c1744ec140e889bfa55a6e34bc895b0822d
Yasmina Bouhriz El Bouhlali <yasmina.bouhriz-el-bouhlali@etudiant.univ-reims.fr>
2024-01-11 14:15:48 +0100

    Ajout de 3 controleurs update, create, delete, dans UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Cassandra\Type\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -39,5 +40,29 @@ class UserController extends AbstractController
         return $this->render('user/show.html.twig', [
             'user' => $user,
         ]);
+    }
+
+    #[Route('/user/update/{id<\d+>}', name: 'app_user_update')]
+    public function update(User $user): Response
+    {
+        return $this->render('user/update.html.twig', [
+            'user' => $user,
+        ]);
+
+    }
+
+    #[Route('/user/create', name: 'app_user_create')]
+    public function create(): Response
+    {
+        return $this->render('user/create.html.twig');
+    }
+
+    #[Route('/user/delete/{id<\d+>}', name: 'app_user_delete')]
+    public function delete(User $user): Response
+    {
+        return $this->render('user/delete.html.twig', [
+            'user' => $user,
+        ]);
+
     }
 }
