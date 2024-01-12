7a08b9ec182709f12239b511902037188b9c5aae
Yasmina Bouhriz El Bouhlali <yasmina.bouhriz-el-bouhlali@etudiant.univ-reims.fr>
2024-01-12 19:12:46 +0100

    Ajout de l'action show() pour les alleregenes et la vue associé

--- src/Controller/AllergeneController.php
+++ src/Controller/AllergeneController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Allergene;
 use App\Repository\AllergeneRepository;
+use App\Repository\IngredientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +14,21 @@ class AllergeneController extends AbstractController
     #[Route('/allergene', name: 'app_allergene')]
     public function index(AllergeneRepository $allergeneRepository): Response
     {
-        $Allergenes = $allergeneRepository->findBy([],['nom'=>'ASC']);
+        $Allergenes = $allergeneRepository->findBy([], ['nom' => 'ASC']);
 
         return $this->render('allergene/index.html.twig', [
             'Allergenes' => $Allergenes,
+        ]);
+    }
+
+    #[Route('/allergene/{id}', name: 'app_allergene_show')]
+    public function show(Allergene $allergene, IngredientRepository $ingredientRepository): Response
+    {
+        $ingredients = $ingredientRepository->etapes($allergene->getId());
+
+        return $this->render('allergene/show.html.twig', [
+            'ingredients' => $ingredients,
+            'allergene' => $allergene,
         ]);
     }
 }
