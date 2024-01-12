8fc4135d37ef622cc79cfe9533adca0619e1ded9
line martinet <line.martinet@etudiant.univ-reims.fr>
2024-01-12 21:08:25 +0100

    modification des routes des actions create et creatOk pour y ajouter un paramettre id. et modication des action concernées pour y ajouter les nouveaux paramettres aux redirections

--- src/Controller/EtapeController.php
+++ src/Controller/EtapeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Etape;
+use App\Entity\Recette;
 use App\Form\EtapeType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,8 +15,8 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class EtapeController extends AbstractController
 {
     #[IsGranted('ROLE_USER')]
-    #[Route('/etape/create', name: 'app_etape_create')]
-    public function create(Request $request, EntityManagerInterface $entityManager)
+    #[Route('/etape/create/{id<\d+>}', name: 'app_etape_create')]
+    public function create(Recette $recette, Request $request, EntityManagerInterface $entityManager)
     {
         $etape = new Etape();
         $form = $this->createForm(EtapeType::class, $etape);
@@ -25,19 +26,23 @@ class EtapeController extends AbstractController
             $entityManager->persist($etape);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_etape_createOk');
+            return $this->redirectToRoute('app_etape_createOk', parameters: ['id' => $recette->getId()]);
         }
+
 
         return $this->render('etape/create.html.twig', parameters: [
             'etape' => $etape,
             'form' => $form,
+            'recette'=>$recette,
         ]);
     }
 
     #[IsGranted('ROLE_USER')]
-    #[Route('/etape/createOk', name: 'app_etape_createOk')]
-    public function createOk(): Response
+    #[Route('/etape/createOk/{id<\d+>}', name: 'app_etape_createOk')]
+    public function createOk(Recette $recette): Response
     {
-        return $this->render('etape/createOk.html.twig');
+        return $this->render('etape/createOk.html.twig',parameters: [
+        'recette'=>$recette,
+    ]);
     }
 }
