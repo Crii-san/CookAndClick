7a2d7b17b988fe1c573bef38ea23d4bcc954074d
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-13 20:28:21 +0100

    Tests : accessibilité de la page

--- tests/Controller/Categorie/IndexCest.php
+++ tests/Controller/Categorie/IndexCest.php
@@ -8,6 +8,14 @@ use App\Tests\Support\ControllerTester;
 
 class IndexCest
 {
+    public function tryToTest(ControllerTester $I)
+    {
+        $I->amOnPage('/categorie');
+        $I->seeResponseCodeIs(200);
+        $I->seeInTitle('Catégorie');
+        $I->see('Toutes les recettes par catégorie', 'h1');
+    }
+
     public function accessIsRestrictedToAuthenticatedUsers(ControllerTester $I): void
     {
         $categorie = CategorieFactory::createOne();
