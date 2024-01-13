280b01ca548e0aeae343dcbb9ec31c5391ef4e62
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-13 18:56:43 +0100

    Tests : accessibilité de la page

--- tests/Controller/Recette/ShowCest.php
+++ tests/Controller/Recette/ShowCest.php
@@ -1,10 +1,11 @@
 <?php
 
-
 namespace App\Tests\Controller\Recette;
 
+use App\Factory\AllergeneFactory;
 use App\Factory\CategorieFactory;
 use App\Factory\RecetteFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class ShowCest
@@ -14,6 +15,21 @@ class ShowCest
     }
 
     // tests
+    public function tryToTest(ControllerTester $I): void
+    {
+        $categorie = CategorieFactory::createOne();
+        RecetteFactory::createOne(['nom' => 'Pâtes au beurre', 'categorie' => $categorie]);
+
+        $allergene = AllergeneFactory::createOne();
+        $user = UserFactory::createOne(['roles' => ['ROLE_USER'], 'allergene' => $allergene]);
+        $I->amLoggedInAs($user->object());
+
+        $I->amOnPage('/recette/1');
+        $I->seeResponseCodeIs(200);
+        $I->seeInTitle('Pâtes au beurre');
+        $I->see('Pâtes au beurre', 'h1');
+    }
+
     public function accessIsRestrictedToAuthenticatedUsers(ControllerTester $I): void
     {
         $categorie = CategorieFactory::createOne();
