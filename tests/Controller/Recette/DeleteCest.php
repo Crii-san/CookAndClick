9d73a7b0f0f00d0d313709ec13e8d3c4a5e5fca2
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-13 19:45:23 +0100

    Génération du fichier de tests de l'action delete() de RecetteController

---
+++ tests/Controller/Recette/DeleteCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller\Recette;
+
+use App\Tests\Support\ControllerTester;
+
+class DeleteCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
