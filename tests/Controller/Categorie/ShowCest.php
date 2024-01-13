7a622fd904f7e7c6a07a4af024872decf2ab8581
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-13 21:13:08 +0100

    Génération du fichier test pour l'action show() de CategorieControlleur

---
+++ tests/Controller/Categorie/ShowCest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Tests\Controller\Categorie;
+
+use App\Factory\AllergeneFactory;
+use App\Factory\CategorieFactory;
+use App\Factory\RecetteFactory;
+use App\Factory\UserFactory;
+use App\Tests\Support\ControllerTester;
+
+class ShowCest
+{
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
