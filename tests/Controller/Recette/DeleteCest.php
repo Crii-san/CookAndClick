008a4d9da5fa4519662eabb6a239cad20243b8ab
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-13 19:55:24 +0100

    Tests : accessibilité de la page

--- tests/Controller/Recette/DeleteCest.php
+++ tests/Controller/Recette/DeleteCest.php
@@ -3,16 +3,29 @@
 
 namespace App\Tests\Controller\Recette;
 
+use App\Factory\AllergeneFactory;
+use App\Factory\CategorieFactory;
+use App\Factory\RecetteFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
+use Codeception\Util\HttpCode;
 
 class DeleteCest
 {
-    public function _before(ControllerTester $I)
+    public function formShowsRecetteDataBeforeDeleting(ControllerTester $I): void
     {
+        $allergene = AllergeneFactory::createOne();
+        $user = UserFactory::createOne(['roles' => ['ROLE_ADMIN'], 'allergene' => $allergene]);
+        $I->amLoggedInAs($user->object());
+
+        $categorie = CategorieFactory::createOne();
+        RecetteFactory::createOne(['nom' => 'Pâtes au beurre', 'categorie' => $categorie]);
+
+        $I->amOnPage('/recette/delete/1');
+
+        $I->seeInTitle('Suppression de Pâtes au beurre');
+        $I->see('Suppression de la recette : Pâtes au beurre', 'h1');
     }
 
-    // tests
-    public function tryToTest(ControllerTester $I)
-    {
-    }
+
 }
