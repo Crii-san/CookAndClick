0e6cfadd04b03c5516a1216f33ce92835c0a386f
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-13 18:15:37 +0100

    Tests : clique sur la première recette pour accéder à sa page détail

--- tests/Controller/Recette/IndexCest.php
+++ tests/Controller/Recette/IndexCest.php
@@ -1,8 +1,12 @@
 <?php
 
-
 namespace App\Tests\Controller\Recette;
 
+use App\Entity\Allergene;
+use App\Factory\AllergeneFactory;
+use App\Factory\CategorieFactory;
+use App\Factory\RecetteFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class IndexCest
@@ -18,5 +22,22 @@ class IndexCest
         $I->seeResponseCodeIs(200);
         $I->seeInTitle('Toutes les recettes');
         $I->see('Toutes les recettes', 'h1');
+    }
+
+    public function clickFirstLink(ControllerTester $I): void
+    {
+        // Création d'une recette (avec sa catégorie)
+        $categorie = CategorieFactory::createOne();
+        RecetteFactory::createOne(['nom' => 'Pâtes au beurre', 'categorie' => $categorie]);
+
+        // Création d'une User pour accéder à la page détail (avec son allergie)
+        $allergene = AllergeneFactory::createOne();
+        $user = UserFactory::createOne(['roles' => ['ROLE_USER'], 'allergene' => $allergene]);
+        $I->amLoggedInAs($user->object());
+
+        // test
+        $I->amOnPage('/recette');
+        $I->click('.btn-primary');
+        $I->seeCurrentRouteIs('app_recette_show');
     }
 }
