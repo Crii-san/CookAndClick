44b064c45764dac3be8448925ecd9c07e1813878
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-13 19:41:44 +0100

    Tests : bon fonctionnement de la barre de recherche

--- tests/Controller/Acceuil/IndexCest.php
+++ tests/Controller/Acceuil/IndexCest.php
@@ -3,6 +3,8 @@
 
 namespace App\Tests\Controller\Acceuil;
 
+use App\Factory\CategorieFactory;
+use App\Factory\RecetteFactory;
 use App\Tests\Support\ControllerTester;
 
 class IndexCest
@@ -20,4 +22,19 @@ class IndexCest
         $I->see('Les dernières recettes', 'h1');
     }
 
+    public function search(ControllerTester $I): void
+    {
+        $categorie = CategorieFactory::createOne();
+        for ($i = 0; $i < 2; ++$i) {
+            RecetteFactory::createOne(['categorie' => $categorie]);
+        }
+        RecetteFactory::createOne(['nom' => 'Fondant au chocolat', 'categorie' => $categorie]);
+        RecetteFactory::createOne(['nom' => 'Chocolat chaud', 'categorie' => $categorie]);
+
+        $chercheCaractere = 'chocolat';
+
+        $I->amOnPage('/recette?search='.$chercheCaractere);
+        $I->see('Fondant au chocolat');
+        $I->see('Chocolat chaud');
+    }
 }
