ee007b5501348833cd5d34623b2bca6355840119
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-15 15:38:22 +0100

    Tests : tri des allergènes par ordre alphabétique

--- tests/Controller/Allergene/IndexCest.php
+++ tests/Controller/Allergene/IndexCest.php
@@ -1,8 +1,8 @@
 <?php
 
-
 namespace App\Tests\Controller\Allergene;
 
+use App\Factory\AllergeneFactory;
 use App\Tests\Support\ControllerTester;
 
 class IndexCest
@@ -13,5 +13,28 @@ class IndexCest
         $I->seeResponseCodeIs(200);
         $I->seeInTitle('Tous les allergènes');
         $I->see('Tous les allergènes', 'h1');
+    }
+
+    public function controlSortRecette(ControllerTester $I): void
+    {
+        AllergeneFactory::createSequence([
+            ['nom' => 'P'],
+            ['nom' => 'M'],
+            ['nom' => 'Z'],
+            ['nom' => 'A'],
+        ]);
+
+        $expected = [
+            'A',
+            'M',
+            'P',
+            'Z',
+        ];
+
+        $I->amOnPage('/allergene');
+
+        $listRecette = $I->grabMultiple('.allergene');
+
+        $I->assertEquals($expected, $listRecette, "L'ordre est incorrect");
     }
 }
