ebe94d54785ed362c3cd559e054170546305aa1b
line martinet <line.martinet@etudiant.univ-reims.fr>
2024-01-16 18:51:27 +0100

    Ajout de l'action add() dans IngredientController.php

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Etape;
+use App\Entity\Recette;
+use App\Form\IngredientListType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class IngredientController extends AbstractController
 {
@@ -13,6 +19,26 @@ class IngredientController extends AbstractController
     {
         return $this->render('ingredient/index.html.twig', [
             'controller_name' => 'IngredientController',
+        ]);
+    }
+
+    #[IsGranted('ROLE_USER')]
+    #[Route('/etape/add/{id<\d+>}/{id<\d+>}', name: 'app_recette_create')]
+    public function add(Recette $recette, Etape $etape, Request $request, EntityManagerInterface $entityManager)
+    {
+        $form = $this->createForm(IngredientListType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $etape->addIngredient($ingredient);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_etape_create', parameters: ['id' => $recette->getId()]);
+        }
+
+        return $this->render('recette/create.html.twig', parameters: [
+            'recette' => $recette,
+            'form' => $form,
         ]);
     }
 }
