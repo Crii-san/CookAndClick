8c93d9eafaabf0ce16f8de0533e36b3ad12fe98b
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-16 08:42:29 +0100

    Tests : accès restraint aux administrateurs

--- tests/Controller/User/IndexCest.php
+++ tests/Controller/User/IndexCest.php
@@ -1,18 +1,20 @@
 <?php
 
-
 namespace App\Tests\Controller\User;
 
+use App\Factory\AllergeneFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class IndexCest
 {
-    public function _before(ControllerTester $I)
+    public function accessIsRestrictedToAdminUsers(ControllerTester $I): void
     {
-    }
+        $allergene = AllergeneFactory::createOne();
+        $user = UserFactory::createOne(['roles' => ['ROLE_USER'], 'allergene' => $allergene]);
+        $I->amLoggedInAs($user->object());
 
-    // tests
-    public function tryToTest(ControllerTester $I)
-    {
+        $I->amOnPage('/user');
+        $I->see('Vous n\'avez pas la permission d\'accéder à cette page.', 'p');
     }
 }
