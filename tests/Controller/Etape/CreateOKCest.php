04fc483dcfa5dbfb71f10852014a45a2c43b4205
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-19 11:15:39 +0100

    Tests : accessibilité de la page de validation d'une étape

--- tests/Controller/Etape/CreateOKCest.php
+++ tests/Controller/Etape/CreateOKCest.php
@@ -1,18 +1,28 @@
 <?php
 
-
 namespace App\Tests\Controller\Etape;
 
+use App\Factory\AllergeneFactory;
+use App\Factory\CategorieFactory;
+use App\Factory\RecetteFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class CreateOKCest
 {
-    public function _before(ControllerTester $I)
-    {
-    }
-
     // tests
     public function tryToTest(ControllerTester $I)
     {
+        $categorie = CategorieFactory::createOne();
+        RecetteFactory::createOne(['nom' => 'Fondant au chocolat', 'categorie' => $categorie]);
+
+        $allergene = AllergeneFactory::createOne();
+        $user = UserFactory::createOne(['roles' => ['ROLE_USER'], 'allergene' => $allergene]);
+        $I->amLoggedInAs($user->object());
+
+        $I->amOnPage('/etape/createOk/1');
+        $I->seeResponseCodeIs(200);
+        $I->seeInTitle('Fondant au chocolat');
+        $I->see('Nouvelle étape pour la recette : Fondant au chocolat', 'h1');
     }
 }
