f861540f4a3130712a9af72a615be49d63e217e1
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-19 11:13:06 +0100

    Génération d'une classe de « Cest » pour l'action « createOk() » du « EtapeController »

---
+++ tests/Controller/Etape/CreateOKCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller\Etape;
+
+use App\Tests\Support\ControllerTester;
+
+class CreateOKCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
