b8588ac6f4ac94ae918e7946db903e55255a697d
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2024-01-19 11:04:38 +0100

    Tests : accessibilité de la page

--- tests/Controller/Etape/CreateCest.php
+++ tests/Controller/Etape/CreateCest.php
@@ -1,18 +1,28 @@
 <?php
 
-
 namespace App\Tests\Controller\Etape;
 
+use App\Factory\AllergeneFactory;
+use App\Factory\CategorieFactory;
+use App\Factory\RecetteFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class CreateCest
 {
-    public function _before(ControllerTester $I)
-    {
-    }
-
     // tests
-    public function tryToTest(ControllerTester $I)
+    public function form(ControllerTester $I)
     {
+        $categorie = CategorieFactory::createOne();
+        RecetteFactory::createOne(['nom' => 'Fondant au chocolat', 'categorie' => $categorie]);
+
+        $allergene = AllergeneFactory::createOne();
+        $user = UserFactory::createOne(['roles' => ['ROLE_USER'], 'allergene' => $allergene]);
+        $I->amLoggedInAs($user->object());
+
+        $I->amOnPage('/etape/create/1');
+        $I->seeResponseCodeIs(200);
+        $I->seeInTitle('Fondant au chocolat');
+        $I->see("Création d'une nouvelle étape pour la recette : Fondant au chocolat", 'h1');
     }
 }
